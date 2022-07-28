c94d1fef8662b7015f2890418220c2fe3cfb9dab
서혜준 (Riel) <90256209+shj718@users.noreply.github.com>
2022-07-28 20:08:47 +0900

    Update hyejun.cpp

--- 백준_코테연습/다이나믹프로그래밍/이동하기/hyejun.cpp
+++ 백준_코테연습/다이나믹프로그래밍/이동하기/hyejun.cpp
@@ -6,10 +6,6 @@ using namespace std;
 int a[1001][1001];
 int d[1001][1001];
 
-int max3(int x, int y, int z) {
-	return max(x, max(y, z));
-}
-
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     // input
@@ -23,7 +19,7 @@ int main() {
     // dp
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            d[i][j] = max3(d[i - 1][j], d[i][j - 1], d[i - 1][j - 1]) + a[i][j];
+            d[i][j] = max(d[i - 1][j], d[i][j - 1]) + a[i][j];
         }
     }
     // output
