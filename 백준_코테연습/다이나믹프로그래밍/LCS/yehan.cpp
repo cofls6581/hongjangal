101c8fbc99a1def3279792878231df9b7059d1a9
박예한 <55950561+pyh-dotcom@users.noreply.github.com>
2022-09-08 15:32:07 +0900

    Create yehan.cpp

---
+++ 백준_코테연습/다이나믹프로그래밍/LCS/yehan.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int dp[1001][1001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	// 입력 및 초기화
+	string a, b;
+	cin >> a >> b;
+	int n = a.size();
+	int m = b.size();
+
+
+	// 로직
+	a = " " + a;
+	b = " " + b;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (a[i] == b[j]) {
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			}
+			else {
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+		}
+	}
+
+
+	// 출력 및 종료
+	cout << dp[n][m] << '\n';
+	return 0;
+}
