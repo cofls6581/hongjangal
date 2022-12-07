315e9145c289f4ec3019cf058114145fd02dab70
이채린 (Bryn) <67696767+cofls6581@users.noreply.github.com>
2022-12-07 23:39:44 +0900

    Create chaerin.cpp

---
+++ 복습/백준_코테기초/수학/소수 찾기/chaerin.cpp
@@ -0,0 +1,37 @@
+/*
+처음에는 Input을 받은 후 바로 소수 판별 로직을 짜려고 했다가
+소수 구하는 로직들을 활용해 소수인지아닌지 정보를 저장하는 배열을 미리 세팅한뒤
+인풋을 받아 해당 인풋이 소수인지아닌지 배열의 값을 읽어와 판별하는 방식으로 로직 수정.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int N,num;
+int prime[1001]={0,};
+
+int main () {
+    ios_base::sync_with_stdio(0); cin.tie(NULL);
+    //input
+    cin>>N;
+
+    //logic 
+    prime[1]=1;
+    for(int i=2;i<=1000;i++) {
+		if (prime[i]) continue;
+		for (int j=i+i; j<=1000; j+=i) {
+			prime[j] = 1;
+		}
+	}
+
+    int ans=0;
+    for(int i=0;i<N;++i){
+        cin>>num;
+        if(!prime[num]) ans++;
+    }
+    
+    //output
+    cout<<ans<<'\n';
+    return 0;
+}
